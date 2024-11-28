f479bc1624df72d4ea56da63c29d21ad0b1251ef
nour soula <nour.soula@enicar.ucar.tn>
2024-11-28 16:14:46 +0100

    Updated seeding script for database initialization

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,24 +1,25 @@
-import { db } from "@vercel/postgres";
+// import { db } from "@vercel/postgres";
 
-const client = await db.connect();
+// const client = await db.connect();
 
-async function listInvoices() {
-  const data = await client.sql`
-    SELECT invoices.amount, customers.name
-    FROM invoices
-    JOIN customers ON invoices.customer_id = customers.id
-    WHERE invoices.amount = 666;
-  `;
+// async function listInvoices() {
+//   const data = await client.sql`
+//     SELECT invoices.amount, customers.name
+//     FROM invoices
+//     JOIN customers ON invoices.customer_id = customers.id
+//     WHERE invoices.amount = 666;
+//   `;
 
-  return data.rows; // Returns the rows from the query result.
-}
+//   return data.rows; // Returns the rows from the query result.
+// }
 
-export async function GET() {
-  try {
-    // Call the function to list invoices and return the data.
-    return Response.json(await listInvoices());
-  } catch (error) {
-    // Handle any errors and return a 500 status code with the error details.
-    return Response.json({ error: error.message }, { status: 500 });
-  }
-}
+// export async function GET() {
+//   try {
+//     // Call the function to list invoices and return the data.
+//     const invoices = await listInvoices();
+//     return Response.json({ invoices });
+//   } catch (error) {
+//     // Handle any errors and return a 500 status code with the error details.
+//     return Response.json({ error: error.message }, { status: 500 });
+//   }
+// }
