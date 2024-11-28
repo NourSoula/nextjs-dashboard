07d290cec20803ad7d434923491944ab884ac251
nour soula <nour.soula@enicar.ucar.tn>
2024-11-28 16:19:13 +0100

    Updated seeding script for database initialization

--- app/query/route.ts
+++ app/query/route.ts
@@ -3,23 +3,24 @@
 // const client = await db.connect();
 
 // async function listInvoices() {
-//   const data = await client.sql`
+// 	const data = await client.sql`
 //     SELECT invoices.amount, customers.name
 //     FROM invoices
 //     JOIN customers ON invoices.customer_id = customers.id
 //     WHERE invoices.amount = 666;
 //   `;
 
-//   return data.rows; // Returns the rows from the query result.
+// 	return data.rows;
 // }
 
-// export async function GET() {
-//   try {
-//     // Call the function to list invoices and return the data.
-//     const invoices = await listInvoices();
-//     return Response.json({ invoices });
-//   } catch (error) {
-//     // Handle any errors and return a 500 status code with the error details.
-//     return Response.json({ error: error.message }, { status: 500 });
-//   }
-// }
+export async function GET() {
+  return Response.json({
+    message:
+      'Uncomment this file and remove this line. You can delete this file when you are finished.',
+  });
+  // try {
+  // 	return Response.json(await listInvoices());
+  // } catch (error) {
+  // 	return Response.json({ error }, { status: 500 });
+  // }
+}
